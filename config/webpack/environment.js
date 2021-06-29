ad46bee02edfd896eb60f69629f1e6b4b7be6102
Michela Iacobucci <miacobucci17@gmail.com>
2021-06-29 16:31:27 -0700

    bug fixes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,9 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'defualt']
-}))
+    Popper: ['popper.js', 'default']
+  }))
 module.exports = environment
